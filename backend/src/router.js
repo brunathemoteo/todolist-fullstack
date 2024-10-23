b599011bd6df6c83cb09636b82bc94ea119ca37b
brunathemoteo <brunathemoteo@gmail.com>
2024-10-23 01:18:25 -0300

    finished

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,12 +5,15 @@ const tasksMiddleware = require("./middlewares/taskMiddleware");
 const router = express.Router();
 
 router.get("/tasks", tasksController.getAll);
+
 router.post(
 	"/tasks",
 	tasksMiddleware.validateFieldTitle,
 	tasksController.createTask,
 );
+
 router.delete("/tasks/:id", tasksController.deleteTask);
+
 router.put(
 	"/tasks/:id",
 	tasksMiddleware.validateFieldTitle,
